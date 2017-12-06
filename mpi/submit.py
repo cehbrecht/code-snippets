4fd64e89af540c99ce048f39efbb4d119f3ff867
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-12-06 14:31:01 +0100

    added submit.py

---
+++ mpi/submit.py
@@ -0,0 +1,27 @@
+import time
+from mpi4py import MPI
+from mpi4py.futures import MPIPoolExecutor
+
+
+def sleep(seconds, name, mymap):
+    print("lets get some sleep ...")
+    print('seconds: {}, name: {}', seconds, name)
+    print(mymap)
+    # mpi
+    comm = MPI.COMM_WORLD
+    # process start number 0, 1, ...
+    rank = comm.Get_rank()
+    # max number of processes
+    size = comm.Get_size()
+    # sleep code
+    tic = time.time()
+    time.sleep(seconds)
+    tac = time.time()
+    print("Completion time [#{}/{}]: {} seconds".format(rank, size, tac - tic))
+    return mymap
+
+
+if __name__ == '__main__':
+    with MPIPoolExecutor(max_workers=4) as executor:
+        future = executor.submit(sleep, seconds=1, name='sleep', mymap={'one': 1, 'two': 2})
+        print(future.result())
