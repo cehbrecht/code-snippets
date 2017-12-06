44ed0d783d1e42e8209dd17d59443e28d4bb7904
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-12-06 14:03:47 +0100

    added worker.py

---
+++ mpi/worker.py
@@ -0,0 +1,23 @@
+import time
+import random
+from mpi4py import MPI
+
+
+def sleep():
+    print("lets get some sleep ...")
+    # mpi
+    # comm = MPI.COMM_WORLD
+    comm = MPI.Comm.Get_parent()
+    # process start number 0, 1, ...
+    rank = comm.Get_rank()
+    # max number of processes
+    size = comm.Get_size()
+    # sleep code
+    tic = time.time()
+    time.sleep(random.randint(1, 3))
+    tac = time.time()
+    print("Completion time [#{}/{}]: {} seconds".format(rank, size, tac - tic))
+
+
+if __name__ == '__main__':
+    sleep()
