f40d95da51d568ef2d9edf3385519569b6fd1185
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-12-06 13:08:25 +0100

    update sleep

--- mpi/sleep.py
+++ mpi/sleep.py
@@ -3,12 +3,17 @@ from mpi4py import MPI
 
 
 def run():
+    # mpi
     comm = MPI.COMM_WORLD
+    # process start number 0, 1, ...
     rank = comm.Get_rank()
+    # max number of processes
+    size = comm.Get_size()
+    # sleep code
     tic = time.time()
     time.sleep(1)
     tac = time.time()
-    print("Completion time [#{}]: {} seconds".format(rank, tac - tic))
+    print("Completion time [#{}/{}]: {} seconds".format(rank, size, tac - tic))
 
 
 if __name__ == '__main__':
