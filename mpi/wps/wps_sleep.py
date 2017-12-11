5034bc46ed05efababd96cda0418bcc8e0ccc9ed
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-12-11 13:35:42 +0100

    using MPIPoolExecutor

--- mpi/wps/wps_sleep.py
+++ mpi/wps/wps_sleep.py
@@ -1,17 +1,43 @@
 from pywps import Process, LiteralInput, LiteralOutput
 from pywps.app.Common import Metadata
 
-import sys
+
 import os
-import json
-import dill
-import subprocess
+import time
 from mpi4py import MPI
+from mpi4py.futures import MPIPoolExecutor
+
+from pywps.tests import WpsClient, WpsTestResponse
+from pywps import Service
+from pywps.tests import assert_response_success
 
 import logging
 LOGGER = logging.getLogger("PYWPS")
 
 MODULE_PATH = os.path.abspath(os.path.dirname(__file__))
+
+
+def sleep(seconds=1):
+    tic = time.time()
+
+    print('zzzZ')
+    time.sleep(seconds)
+    print('... awake')
+
+    # mpi
+    comm = MPI.COMM_WORLD
+    # process start number 0, 1, ...
+    rank = comm.Get_rank()
+    # max number of processes
+    size = comm.Get_size()
+    # notify about completion
+    tac = time.time()
+    msg = "Completion time [#{}/{}]: {} seconds\n".format(rank, size, tac - tic)
+    print(msg)
+    with open(os.path.join(MODULE_PATH, 'sleep.log'), 'a') as fp:
+        fp.write(msg)
+    # raise Exception("Akari!")
+    return "sleeping done"
 
 
 class Sleep(Process):
@@ -43,69 +69,20 @@ class Sleep(Process):
         )
 
     def _handler(self, request, response):
-        with open('request.json', 'w') as fp:
-            fp.write(request.json)
-        with open('response.dump', 'w') as fp:
-            dill.dump(response, fp)
-        # mpi_launcher()
-        # raise Exception("current dir=%s" % os.path.abspath(os.path.curdir))
-        # raise Exception("which mpiexec=%s" % sys.executable)
-        try:
-            bin_dir = os.path.dirname(sys.executable)
-            subprocess.check_output(
-                [os.path.join(bin_dir, 'mpiexec'), '-n', '1',
-                 sys.executable, os.path.join(MODULE_PATH, 'wps_sleep.py')],
-                stderr=subprocess.STDOUT)
-        except subprocess.CalledProcessError as e:
-            LOGGER.exception("mpi failed")
-            raise
-        with open('response.dump', 'r') as fp:
-            response = dill.load(fp)
+        if 'delay' in request.inputs:
+            seconds = request.inputs['delay'][0].data
+        else:
+            seconds = 1
+        response.update_status('PyWPS Process started. Waiting...', 50)
+        with MPIPoolExecutor(max_workers=None, path=[MODULE_PATH]) as executor:
+            # future = executor.submit(time.sleep, 1)
+            future = executor.submit(sleep, seconds=seconds)
+            print(future.result())
+        response.outputs['sleep_output'].data = 'done sleeping'
         return response
 
-# the processing function
-
-
-def sleep(request, response):
-    import time
-
-    tic = time.time()
-
-    if 'delay' in request.inputs:
-        sleep_delay = request.inputs['delay'][0].data
-    else:
-        sleep_delay = 10
-
-    time.sleep(sleep_delay)
-    response.update_status('PyWPS Process started. Waiting...', 50)
-    print('zzzZ')
-    time.sleep(sleep_delay)
-    response.outputs['sleep_output'].data = 'done sleeping'
-    print('... awake')
-
-    # mpi
-    comm = MPI.COMM_WORLD
-    # process start number 0, 1, ...
-    rank = comm.Get_rank()
-    # max number of processes
-    size = comm.Get_size()
-    # notify about completion
-    tac = time.time()
-    msg = "Completion time [#{}/{}]: {} seconds\n".format(rank, size, tac - tic)
-    print(msg)
-    with open(os.path.join(MODULE_PATH, 'sleep.log'), 'a') as fp:
-        fp.write(msg)
-    # raise Exception("Akari!")
-
-    return response
 
 # unit test
-
-
-from pywps.tests import WpsClient, WpsTestResponse
-from pywps import Service
-from pywps.tests import assert_response_success
-
 
 class WpsTestClient(WpsClient):
 
@@ -128,22 +105,3 @@ def test_wps_sleep():
         datainputs=datainputs)
     print(resp.response)
     assert_response_success(resp)
-
-# mpi launcher
-
-
-def mpi_launcher():
-    from pywps import WPSRequest
-    request = WPSRequest()
-    with open('request.json', 'r') as fp:
-        request.json = json.load(fp)
-    with open('response.dump', 'r') as fp:
-        response = dill.load(fp)
-    # do the job
-    response = sleep(request, response)
-    with open('response.dump', 'w') as fp:
-        dill.dump(response, fp)
-
-
-if __name__ == '__main__':
-    mpi_launcher()
