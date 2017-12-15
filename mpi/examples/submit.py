bc2dcc87ee86e3ab6e51fb3e29b02c974b9a89a4
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-12-15 17:08:54 +0100

    update submit

--- mpi/examples/submit.py
+++ mpi/examples/submit.py
@@ -25,4 +25,5 @@ if __name__ == '__main__':
     # with MPIPoolExecutor(max_workers=4) as executor:
     with MPIPoolExecutor() as executor:
         future = executor.submit(sleep, seconds=1, name='sleep', mymap={'one': 1, 'two': 2})
-        print(future.result())
+    assert future.done()
+    print(future.result())
